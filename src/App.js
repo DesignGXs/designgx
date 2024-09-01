c003ed17ccaec331b37dbbe1e9699dab35e698e7
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2024-09-01 23:36:10 +0530

    Animations added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from 'react'
 import Home from './components/Home'
 import Service from './components/Service'
 import Contact from './components/Contact'
-
+import Nav from './components/Navbar'
+import Footer from './components/Footer'
+import TeamPage from './components/Team'
+ import Cursor from './components/Cursor' 
 const App = () => {
   return (
     <div>
+      <Cursor/>
+      <Nav/>
       <Home/>
       <Service/>
-      <Contact/>
+      <TeamPage/>
+      <Contact/> 
+      <Footer/> 
       <h1 style={{textAlign:'center'}}>@ DesignGX 2024</h1>
     </div>
   )
